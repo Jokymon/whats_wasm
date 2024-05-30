1b86a6b71f425128c888ebd97218b113a9eba5a0
Silvan Wegmann <silvan.wegmann@hslu.ch>
2024-05-30 19:32:32 +0000

    Change intro example to C

--- 02_intro.c
+++ 02_intro.c
@@ -1,11 +1,5 @@
 #include <emscripten.h>
 
-extern "C" {
-// We focus on C++ code in these examples. However, since C++ uses "name-mangling" to
-// prevent name collisions on overloaded functions and methods, the exported names
-// are hard to identify in WASM/Javascript. To create better readable names, we use
-// 'extern "C"' to prevent name mangling of the exported functions.
-
 // In this example we only compile a simple library (which will be turned into a
 // WASM module), containing a single function. Since this function isn't used anywhere
 // else on the C/C++ side, Emscripten would optimize it away and remove it. The
@@ -13,6 +7,4 @@ extern "C" {
 EMSCRIPTEN_KEEPALIVE
 int my_add(int a, int b) {
     return a + b;
-}
-
 }
--- Makefile
+++ Makefile
@@ -3,9 +3,8 @@ all: build section_2 section_3 section_20
 build:
 	mkdir build
 
-section_2: 02_intro.cpp
-	emcc -s WASM=1 -s EXPORTED_RUNTIME_METHODS='["cwrap"]' 02_intro.cpp -o build/02_intro.js
-	#emcc 02_intro.cpp -o build/02_intro.html
+section_2: 02_intro.c
+	emcc -s WASM=1 -s EXPORTED_RUNTIME_METHODS='["cwrap"]' 02_intro.c -o build/02_intro.js
 
 section_3: 03_bigger_example.c
 	emcc -O2 -s USE_SDL=2 -s USE_SDL_IMAGE=2 -s SDL2_IMAGE_FORMATS='["png"]' --preload-file 03_assets 03_bigger_example.c -o build/03_bigger_example.html
